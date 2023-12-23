1e5bb2e34ce5b008c04620693c54bab7e4ac9715
Loreq97 <ceziu1997@gmail.com>
2023-12-23 17:42:59 +0100

    Cascade gem match

--- scenes/gem.gd
+++ scenes/gem.gd
@@ -18,7 +18,7 @@ enum GEM_TYPE_E {RED, BLUE, YELLOW, GREEN}
 
 # GLOBALS
 var position_changed: bool = false
-var marked_to_be_destroyed: bool = false
+var marked_to_be_deleted: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,7 +30,7 @@ func _process(_delta):
 	if position_changed:
 		var target: Vector2 = _calculate_gem_position_on_scene()
 		var tween := create_tween()
-		tween.tween_property(self, "position", target, .4)
+		tween.tween_property(self, "position", target, .4).set_trans(Tween.TRANS_QUART)
 		tween.connect("finished", _transition_after_select_is_finished)
 		position_changed = false
 		selected = false
@@ -39,7 +39,6 @@ func _process(_delta):
 	else:
 		$AnimationPlayer.stop()
 	$MagicCircle.visible = selected
-	
 
 func _to_string():
 	return str("[GEM: x-> ", board_x, ", y-> ", board_y, ", color-> ", gem_type, "]")
@@ -50,18 +49,16 @@ func setup_gem_color():
 	gem_type = GEM_TYPE_E.values()[randi() % GEM_TYPE_E.size()]
 	_adjust_gem_color()
 
-
 func setup_gem_position_on_board(x: int, y: int, init: bool = false):
 	board_x = x
 	board_y = y
 	if not init:
 		position_changed = true
 
-
 func destroy():
 	$Sprite2D.visible = false
 	$Particle.emitting = true
-
+	marked_to_be_deleted = true
 
 func _calculate_gem_position_on_scene():
 	# Position on board + offset from sprite center + margin
@@ -72,6 +69,7 @@ func _calculate_gem_position_on_scene():
 	
 func _destroy():
 	emit_signal("gem_destroyed", Vector2(board_x, board_y))
+	queue_free()
 
 func _adjust_gem_color():
 	if gem_type == GEM_TYPE_E.RED:
@@ -87,11 +85,9 @@ func _adjust_gem_color():
 		$Sprite2D.modulate = Color(1, 1, 0)
 		$Particle.color = Color(1, 1, 0)
 
-
 # EVENTS
 func _transition_after_select_is_finished():
 	emit_signal("gem_finished_transition")
-
 
 func _input_event_handle(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
--- scenes/gems_container.gd
+++ scenes/gems_container.gd
@@ -5,7 +5,19 @@ class_name GemContainer
 enum GEM_DIRECTION {LEFT, RIGHT, TOP, BOTTOM}
 
 var BOARD_SIZE = 8
-var REVERT = []
+
+var revert_gems = []
+var destroyed = {}
+
+func _prepare_gem(v: Vector2):
+	var gem = preload("res://scenes/gem.tscn").instantiate()
+	gem.setup_gem_color()
+	gem.setup_gem_position_on_board(v.x, v.y, true)
+	gem.connect("gem_finished_transition", _handle_gem_transition_finished)
+	gem.connect("gem_selected", _handle_gem_selection)
+	gem.connect("gem_destroyed", _handle_gem_destroyed)
+	
+	return gem
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,9 +29,7 @@ func _ready():
 	
 	for y in range(BOARD_SIZE):
 		for x in range(BOARD_SIZE):
-			var gem = preload("res://scenes/gem.tscn").instantiate()
-			gem.setup_gem_color()
-			gem.setup_gem_position_on_board(x, y, true)
+			var gem = _prepare_gem(Vector2(x, y))
 			
 			while true:
 				var left_neighbor: Gem = get_neighbor_gem(gem, GemContainer.GEM_DIRECTION.LEFT)
@@ -38,14 +48,18 @@ func _ready():
 			
 				break
 			
-			gem.connect("gem_finished_transition", _handle_gem_transition_finished)
-			gem.connect("gem_selected", _handle_gem_selection)
 			add_child(gem)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+	#var qq = get_children().filter(func(g): return g.board_x == 6)
+	#print("tst")
 
+func generate_gem_to_fill_board(board_x: int, board_y: int):
+	var gem = _prepare_gem(Vector2(board_x, -1))
+	add_child(gem)
+	gem.setup_gem_position_on_board(board_x, board_y)
 
 func get_neighbor_gem(gem: Gem, direction: GEM_DIRECTION):
 	var target: Array
@@ -77,7 +91,13 @@ func swap_gems_position(gem_1: Gem, gem_2: Gem):
 	var target_y = gem_2.board_y
 	gem_1.setup_gem_position_on_board(target_x, target_y)
 	gem_2.setup_gem_position_on_board(source_x, source_y)
-	
+
+func is_board_filled():
+	var children = get_children()
+	var cond1 = children.filter(func(g): return g.marked_to_be_deleted).is_empty()
+	var cond2 = len(children) == BOARD_SIZE ** 2
+	return cond1 and cond2
+
 func check_for_matches():
 	var gem1: Gem
 	var gem2: Gem
@@ -107,14 +127,45 @@ func check_for_matches():
 	return to_be_destroyed.values()
 
 # EVENTS
-func _handle_gem_transition_finished():
-	if REVERT:
-		swap_gems_position(REVERT[0], REVERT[1])
-		REVERT = []
-	else:
-		for gem_to_destroy in check_for_matches():
-			gem_to_destroy.destroy()
+func _handle_gem_destroyed(v: Vector2):
+	if not destroyed.is_empty():
+		destroyed.erase(v)
+		if destroyed.is_empty():
+			# All gems have finished transition/destruction
+			var gem_count_in_column = []
+			for column in range(BOARD_SIZE):
+				var gems_in_column = get_children().filter(func(g): return g.board_x == column and not g.marked_to_be_deleted)
+				gems_in_column.sort_custom(func(a, b): return a.board_y > b.board_y)
+				gem_count_in_column.append(gems_in_column.size())
+				if len(gems_in_column) == BOARD_SIZE:
+					continue
+				for gem_idx in range(gems_in_column.size()):
+					if gems_in_column.size() - 1 == gem_idx:
+						break
+					var current_gem: Gem = gems_in_column[gem_idx]
+					if gem_idx == 0 and current_gem.board_y != BOARD_SIZE - 1:
+						current_gem.setup_gem_position_on_board(current_gem.board_x, BOARD_SIZE - 1)
+					var next_gem: Gem = gems_in_column[gem_idx + 1]
+					if current_gem.board_y - next_gem.board_y > 1:
+						next_gem.setup_gem_position_on_board(current_gem.board_x, current_gem.board_y - 1)
+			for idx in range(gem_count_in_column.size()):
+				if gem_count_in_column[idx] == BOARD_SIZE:
+					continue
+				for i in range(BOARD_SIZE - gem_count_in_column[idx] - 1, -1, -1):
+					generate_gem_to_fill_board(idx, i)
 
+func _handle_gem_transition_finished():
+	if revert_gems:
+		swap_gems_position(revert_gems[0], revert_gems[1])
+		revert_gems = []
+	else:
+		if not is_board_filled():
+			return
+		var matches = check_for_matches()
+		for gem_to_destroy in matches:
+			destroyed[Vector2(gem_to_destroy.board_x, gem_to_destroy.board_y)] = gem_to_destroy
+			gem_to_destroy.destroy()
+		
 func _handle_gem_selection(gem: Gem):
 	var result = get_children().filter(func(g): return g.selected)
 	if len(result) > 1:
@@ -123,7 +174,7 @@ func _handle_gem_selection(gem: Gem):
 				swap_gems_position(gem, target)
 				var gems_to_destroy = check_for_matches()
 				if not gems_to_destroy:
-					REVERT.append(gem)
-					REVERT.append(target)
+					revert_gems.append(gem)
+					revert_gems.append(target)
 			else:
 				target.selected = false
