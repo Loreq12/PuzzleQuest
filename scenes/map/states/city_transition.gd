2ce1cff98a6534387fbc684db78f1a9bd8721559
Loreq97 <ceziu1997@gmail.com>
2024-02-28 07:45:29 +0100

    move interaction group to city scene level

--- scenes/map/states/city_transition.gd
+++ scenes/map/states/city_transition.gd
@@ -4,6 +4,6 @@ class_name MapCityTransition
 @export var cities: CityContainer
 
 func Enter():
-	get_tree().call_group("city_on_map", "disable_interation")
+	get_tree().call_group("interaction_on_travel", "disable_interation")
 	await cities.transition_player_to_city()
-	get_tree().call_group("city_on_map", "enable_interation")
+	get_tree().call_group("interaction_on_travel", "enable_interation")
