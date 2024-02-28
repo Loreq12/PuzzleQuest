442b18023c768ad9350e9669a1f377d1d467b5a5
Loreq97 <ceziu1997@gmail.com>
2024-02-28 07:12:04 +0100

    map travelling

--- scenes/map/map_cities.gd
+++ scenes/map/map_cities.gd
@@ -38,8 +38,15 @@ func _draw():
 			draw_line(city.position, neighbour.position, Color.BLACK, selected_path_width)
 
 func transition_player_to_city():
+	var tween := create_tween()
+	tween.set_parallel(false)
+	tween.set_trans(Tween.TRANS_LINEAR)
+
 	var path = city_graph.get_point_path(current_city.get_index(), selected_city.get_index())
-	print(path)
+	var cities = city_graph.get_id_path(current_city.get_index(), selected_city.get_index())
+	for i in range(1, path.size()):
+		tween.tween_property($"../Player", "position", path[i], 1.5)
+		tween.tween_callback(func (): current_city = get_child(cities[i]))
 
 func _on_city_highlight(city: MapCity):
 	if city == current_city:
